7bf16ec5126d1310eb65bd4fbdc211d468298fdf
whiteofc <whitemusicofc@gmail.com>
2023-08-15 20:48:57 -0300

    bug: Cors problem resolved

--- src/main/java/com/parking82/api/config/WebConfig.java
+++ src/main/java/com/parking82/api/config/WebConfig.java
@@ -4,10 +4,10 @@ import com.parking82.api.services.UserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -35,6 +35,8 @@ public class WebConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http.csrf().disable()
+                .apply(new CorsConfigurer<HttpSecurity>()) 
+                .and()
                 .authorizeHttpRequests(
                         authorizeConfig -> {
 
@@ -54,6 +56,7 @@ public class WebConfig {
                 .logout()
                 .logoutSuccessUrl("/usuario/login")
                 .permitAll()
+                
                 .and().httpBasic()
                 .and().build();
 
